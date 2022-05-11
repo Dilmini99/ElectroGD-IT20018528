e2c7a564d49ac1eae1038e1ac63371b9dc6ed314
Dilmini Athapaththu <88821681+Dilmini99@users.noreply.github.com>
2022-05-11 19:23:20 +0530

    Create Post Request

--- CustomerFront/src/com/CustomerService.java
+++ CustomerFront/src/com/CustomerService.java
@@ -32,3 +32,11 @@ public class CustomerService extends HttpServlet {
 		// TODO Auto-generated method stub
 		
 	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String output = customerObj.insertCustomer(request.getParameter("cName"),      
+				request.getParameter("cAddress"),
+				request.getParameter("cEmail"),
+				request.getParameter("cPhone")); 
+				response.getWriter().write(output);
+	}
