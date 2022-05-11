6c26372013d60e2dbbf708b4aae63051d41bf8cd
Dilmini Athapaththu <88821681+Dilmini99@users.noreply.github.com>
2022-05-11 13:21:51 +0530

    Create Database Server

--- CustomerFront/src/com/Customer.java
+++ CustomerFront/src/com/Customer.java
@@ -25,4 +25,38 @@ public class Customer {
 		return con;
 	}
 
-
+	public String insertCustomer(String cName, String cAddress, String cEmail, String cPhone)  
+	{   
+		String output = ""; 	 
+		try   
+		{    
+			Connection con = connect(); 
+	 
+			if (con == null)    
+			{return "Error while connecting to the database for inserting."; } 
+	 
+			// create a prepared statement 
+			String query = " insert into customer(`cID`,`cName`,`cAddress`,`cEmail`,`cPhone`)" + " values (?, ?, ?, ?, ?)"; 
+			PreparedStatement preparedStmt = con.prepareStatement(query); 
+	 
+			// binding values    
+			 preparedStmt.setInt(1, 0);
+			 preparedStmt.setString(2, cName);
+			 preparedStmt.setString(3, cAddress);
+			 preparedStmt.setString(4, cEmail);
+			 preparedStmt.setString(5, cPhone);
+			
+			// execute the statement    
+			preparedStmt.execute();    
+			con.close(); 
+	   
+			String newCustomer = readCustomer(); 
+			output =  "{\"status\":\"success\", \"data\": \"" + newCustomer + "\"}";    
+		}   
+		catch (Exception e)   
+		{    
+			output =  "{\"status\":\"error\", \"data\": \"Error while inserting the Customer.\"}";  
+			System.err.println(e.getMessage());   
+		} 		
+	  return output;  
+	} 	
