e2ed95cd5a1e55a12797c0077a264fedcaafcce9
Dilmini Athapaththu <88821681+Dilmini99@users.noreply.github.com>
2022-05-12 21:41:16 +0530

    Save

---
+++ CustomerFront/WebContent/Components/valicustomer.js
@@ -0,0 +1,73 @@
+$(document).ready(function() 
+{  
+		$("#alertSuccess").hide();  
+	    $("#alertError").hide(); 
+}); 
+ 
+ 
+// SAVE ============================================ 
+$(document).on("click", "#btnSave", function(event) 
+{  
+	// Clear alerts---------------------  
+	$("#alertSuccess").text("");  
+	$("#alertSuccess").hide();  
+	$("#alertError").text("");  
+	$("#alertError").hide(); 
+ 
+	// Form validation-------------------  
+	var status = validateCustomerForm();  
+	if (status != true)  
+	{   
+		$("#alertError").text(status);   
+		$("#alertError").show();   
+		return;  
+	} 
+ 
+	// If valid------------------------  
+	var type = ($("#hidCustomerIDSave").val() == "") ? "POST" : "PUT"; 
+
+	$.ajax( 
+	{  
+			url : "CustomerService",  
+			type : type,  
+			data : $("#formCustomer").serialize(),  
+			dataType : "text",  
+			complete : function(response, status)  
+			{   
+				onCustomerSaveComplete(response.responseText, status);  
+			} 
+	}); 
+}); 
+
+
+function onCustomerSaveComplete(response, status) 
+{  
+	if (status == "success")  
+	{   
+		var resultSet = JSON.parse(response); 
+
+		if (resultSet.status.trim() == "success")   
+		{    
+			$("#alertSuccess").text("Successfully saved.");    
+			$("#alertSuccess").show(); 
+
+			$("#divCustomerGrid").html(resultSet.data);   
+		} else if (resultSet.status.trim() == "error")   
+		{    
+			$("#alertError").text(resultSet.data);    
+			$("#alertError").show();   
+		} 
+
+	} else if (status == "error")  
+	{   
+		$("#alertError").text("Error while saving.");   
+		$("#alertError").show();  
+	} else  
+	{   
+		$("#alertError").text("Unknown error while saving..");   
+		$("#alertError").show();  
+	} 
+
+	$("#hidCustomerIDSave").val("");  
+	$("#formCustomer")[0].reset(); 
+} 
